9f9b2a11570ba01b0cf784417ac050a2674be165
Arthur <Fokusnik90999@gmail.com>
2024-11-22 00:55:48 +0500

    Initial Commit

---
+++ ToDoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoTest
+//
+//  Created by Артур  Арсланов on 22.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
